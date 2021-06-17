bffd2bc54f677ba30d54f5d55018913651261cd7
Depechie <glenn.versweyveld@gmail.com>
2021-06-17 23:23:23 +0200

    Initial commit

---
+++ MauiBlazorBar/Data/BoundingClientRect.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MauiBlazorBar.Data
+{
+    public class BoundingClientRect
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Top { get; set; }
+        public double Right { get; set; }
+        public double Bottom { get; set; }
+        public double Left { get; set; }
+    }
+}
